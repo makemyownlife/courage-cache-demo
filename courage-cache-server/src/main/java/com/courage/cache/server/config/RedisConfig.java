f37a1bbe47465ae42cf92f6cd9a1f34a6436a9d2
makemyownlife <zhangyong7120180@163.com>
2023-04-12 21:13:01 +0800

    application 配置信息

--- courage-cache-server/src/main/java/com/courage/cache/server/config/RedisConfig.java
+++ courage-cache-server/src/main/java/com/courage/cache/server/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package com.courage.cache.server.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.redisson.config.TransportMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,14 +16,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate redisTemplate = new StringRedisTemplate();
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.setTransportMode(TransportMode.NIO);
+        SingleServerConfig singleServerConfig = config.useSingleServer();
+        singleServerConfig.setAddress("redis://127.0.0.1:6379");
+        singleServerConfig.setPassword("123456");
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
     }
 
 }
