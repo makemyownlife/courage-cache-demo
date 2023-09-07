8872aa27b714f6b723dfac1e6ab1b7a14575dfcd
zhangyong <zhangyong7120180@163.com>
2023-09-07 22:59:29 +0800

    定义 disruptor 的main 函数

---
+++ courage-cache-service/src/main/java/com/courage/cache/service/disruptor/LongEventFactory.java
@@ -0,0 +1,15 @@
+package com.courage.cache.service.disruptor;
+
+import com.lmax.disruptor.EventFactory;
+
+/**
+ * Created by zhangyong on 2023/9/7.
+ */
+public class LongEventFactory implements EventFactory<LongEvent> {
+
+    @Override
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+
+}
---
+++ courage-cache-service/src/main/java/com/courage/cache/service/disruptor/LongEventHandler.java
@@ -0,0 +1,23 @@
+package com.courage.cache.service.disruptor;
+
+
+import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.RingBuffer;
+
+/**
+ * Created by zhangyong on 2023/9/7.
+ */
+public class LongEventHandler implements EventHandler<LongEvent> {
+    /**
+     * Called when a publisher has published an event to the {@link RingBuffer}
+     *
+     * @param longEvent      published to the {@link RingBuffer}
+     * @param sequence       of the event being processed
+     * @param endOfBatch     flag to indicate if this is the last event in a batch from the {@link RingBuffer}
+     * @throws Exception if the EventHandler would like the exception handled further up the chain.
+     */
+    @Override
+    public void onEvent(LongEvent longEvent, long sequence, boolean endOfBatch) throws Exception {
+        System.out.println("Event: " + longEvent);
+    }
+}
