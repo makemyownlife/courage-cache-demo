9fc5b6f71044a562a295524045b05dc8316da5e4
zhangyong <zhangyong7120180@163.com>
2023-09-08 23:07:43 +0800

    longEvent main 2

---
+++ courage-cache-service/src/main/java/com/courage/cache/service/disruptor/LongEventMain2.java
@@ -0,0 +1,38 @@
+package com.courage.cache.service.disruptor;
+
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by zhangyong on 2023/9/8.
+ */
+public class LongEventMain2 {
+
+    public static void handleEvent(LongEvent event, long sequence, boolean endOfBatch) {
+        System.out.println(event);
+    }
+
+    public static void translate(LongEvent event, long sequence, ByteBuffer buffer) {
+        event.set(buffer.getLong(0));
+    }
+
+    public static void main(String[] args) throws Exception {
+        int bufferSize = 1024;
+
+        Disruptor<LongEvent> disruptor = new Disruptor<>(LongEvent::new, bufferSize, DaemonThreadFactory.INSTANCE);
+        disruptor.handleEventsWith(LongEventMain2::handleEvent);
+        disruptor.start();
+
+        RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+        ByteBuffer bb = ByteBuffer.allocate(8);
+        for (long l = 0; true; l++) {
+            bb.putLong(0, l);
+            ringBuffer.publishEvent(LongEventMain2::translate, bb);
+            Thread.sleep(1000);
+        }
+    }
+
+}
